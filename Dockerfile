481256beffd85ae8ebe70355e4164384a52a58bc
Matias <lit.io30303@gmail.com>
2025-04-23 19:13:03 -0400

    primer envio

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM php:8.4.6-apache
+
+COPY /src /var/www/html/
+
+EXPOSE 80
---
+++ src/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing web</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+    <!-- Header - Menu -->
+    <header>
+        <div class="container__header">
+            <div class="logo">
+                <a href="#">
+                    <img src="./assets/images/Logo/logo-magtimus.png" alt="Logo" srcset="">
+                </a>
+            </div>
+            <div class="menu">
+                <nav>
+                    <ul>
+                        <li><a href="#">Inicio</a></li>
+                        <li><a href="#">Sobre</a></li>
+                        <li><a href="#">Servicios</a></li>
+                        <li><a href="#">Contactos</a></li>
+                    </ul>
+                    <a href="#" class="btn__quote">Cotizar</a>
+
+                    <div class="socialMedia">
+                        <a href="#">
+                            <img src="./assets/images/social media/facebook.png" alt="">
+                        </a>
+                        <a href="#">
+                            <img src="./assets/images/social media/instagram.png" alt="">
+                        </a>
+                        <a href="#">
+                            <img src="./assets/images/social media/twitter.png" alt="">
+                        </a>
+                        <a href="#">
+                            <img src="./assets/images/social media/youtube.png" alt="">
+                        </a>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
+</body>
+
+</html>
